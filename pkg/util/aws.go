ee793585ecc001d950c1aa54f44a9331ffa9f547
stephan lindauer <stephanlindauer@posteo.de>
2018-11-20 14:01:56 +0100

    Fix credentials validation to incorporate EC2 role providers

--- pkg/util/aws.go
+++ pkg/util/aws.go
@@ -2,11 +2,14 @@ package util
 
 import (
 	"errors"
+	"net/http"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/rebuy-de/rebuy-go-sdk/cmdutil"
@@ -50,7 +53,7 @@ func (c *AWSProfile) BuildSession(region string) *session.Session {
 }
 
 func (c *AWSProfile) IsValid() bool {
-	if !c.Shared() && !c.Static() {
+	if !c.Shared() && !c.Static() && !c.EC2RoleProvider {
 		return false
 	}
 	return true
@@ -78,19 +81,48 @@ func AttachAWSCredentials(cfg *aws.Config, profile *AWSProfile) error {
 		cfg.WithCredentials(credentials.NewSharedCredentials("", profile.Profile))
 		return nil
 	} else if profile.EC2RoleProvider {
-		cfg.WithCredentials(credentials.NewCredentials(&ec2rolecreds.EC2RoleProvider{}))
+		metadata := ec2metadata.New(session.New(), &aws.Config{
+			HTTPClient: http.DefaultClient,
+		})
+
+		creds := credentials.NewCredentials(&ec2rolecreds.EC2RoleProvider{Client: metadata})
+		if _, err := creds.Get(); err != nil {
+			return err
+		}
+
+		cfg.WithCredentials(creds)
 		return nil
 	}
 
 	return errors.New("No valid AWS credentials found.")
 }
 
-func GetQueueURL(cp client.ConfigProvider, queueName string, awsRegion string) string {
-	stsClient := sts.New(cp)
-	identity, err := stsClient.GetCallerIdentity(nil)
-	if err != nil {
-		log.Error(err)
-		cmdutil.Exit(1)
+func GetQueueURL(cp client.ConfigProvider, queueName string, awsRegion string, profile *AWSProfile) string {
+	accountID := ""
+
+	if profile.Shared() || profile.Static() {
+		stsClient := sts.New(cp)
+		identity, err := stsClient.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+		if err != nil {
+			log.Error(err)
+			cmdutil.Exit(1)
+		}
+		accountID = *identity.Account
+	} else if profile.EC2RoleProvider {
+		metadataClient := ec2metadata.New(cp)
+		info, err := metadataClient.IAMInfo()
+		if err != nil {
+			log.Error(err)
+			cmdutil.Exit(1)
+		}
+
+		arn, err := arn.Parse(info.InstanceProfileArn)
+		if err != nil {
+			log.Error(err)
+			cmdutil.Exit(1)
+		}
+		accountID = arn.AccountID
 	}
-	return "https://sqs." + awsRegion + ".amazonaws.com/" + *identity.Account + "/" + queueName
+
+	return "https://sqs." + awsRegion + ".amazonaws.com/" + accountID + "/" + queueName
 }

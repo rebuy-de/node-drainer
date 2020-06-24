fe16d2a9462ffbd36ab628972f0123b7e908bc28
Sven Walter <s.walter@rebuy.com>
2020-06-24 15:15:18 +0200

    add tests for pod sorting

---
+++ pkg/collectors/pods_sort_test.go
@@ -0,0 +1,57 @@
+package collectors_test
+
+import (
+	"testing"
+
+	"github.com/rebuy-de/node-drainer/v2/pkg/collectors"
+	"github.com/rebuy-de/node-drainer/v2/pkg/collectors/aws/asg"
+	"github.com/rebuy-de/node-drainer/v2/pkg/collectors/aws/ec2"
+)
+
+func TestSortPodByNeedsEviction(t *testing.T) {
+	podsWithEvictionNeeded := collectors.Pod{
+		Instance: collectors.Instance{
+			InstanceID: "xxx",
+			EC2: ec2.Instance{
+				InstanceID: "xxx",
+				State:      "running",
+			},
+			ASG: asg.Instance{
+				ID:        "xxx",
+				Completed: false,
+			},
+		},
+	}
+
+	podsWithEvictionNotNeeded := podsWithEvictionNeeded
+	podsWithEvictionNotNeeded.Instance.ASG.Completed = true
+
+	if !podsWithEvictionNeeded.WantsShutdown() {
+		t.Fatal("sanity check failed. pods should want shutdown")
+	}
+	if podsWithEvictionNotNeeded.WantsShutdown() {
+		t.Fatal("sanity check failed. pods should not want shutdown")
+	}
+
+	pods := collectors.Pods{
+		podsWithEvictionNotNeeded,
+		podsWithEvictionNeeded,
+		podsWithEvictionNotNeeded,
+		podsWithEvictionNeeded,
+	}
+
+	pods.Sort(collectors.PodsByNeedsEviction)
+
+	if !pods[0].NeedsEviction() {
+		t.Error("pod 0 should need eviction")
+	}
+	if !pods[1].NeedsEviction() {
+		t.Error("pod 1 should need eviction")
+	}
+	if pods[2].NeedsEviction() {
+		t.Error("pod 2 should not need eviction")
+	}
+	if pods[3].NeedsEviction() {
+		t.Error("pod 3 should not need eviction")
+	}
+}

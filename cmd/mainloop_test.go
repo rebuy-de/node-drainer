e01b8ab8f390b34797305c6933cb254792684f18
Sven Walter <s.walter@rebuy.com>
2020-08-06 13:30:52 +0200

    fixup! add mainloop tests

--- cmd/mainloop_test.go
+++ cmd/mainloop_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
+	"github.com/rebuy-de/node-drainer/v2/pkg/collectors"
 	"github.com/rebuy-de/node-drainer/v2/pkg/collectors/testdata"
 )
 
@@ -18,10 +19,6 @@ func TestMainLoopLifecycleCompletion(t *testing.T) {
 
 		for _, ec2State := range testdata.AllEC2States {
 			for _, asgState := range testdata.AllASGStates {
-				if ec2State == testdata.EC2Running && asgState != testdata.ASGMissing {
-					continue
-				}
-
 				b.AddInstance(1, testdata.InstanceTemplate{
 					ASG:  asgState,
 					EC2:  ec2State,
@@ -32,15 +29,24 @@ func TestMainLoopLifecycleCompletion(t *testing.T) {
 			}
 		}
 
-		collectors := testdata.GenerateCollectors(t, b.Build())
-		collectors.ASG.(*testdata.ASGClientMock).On("Delete", mock.Anything, mock.Anything).Return(nil)
+		lists := b.Build()
+		coll := testdata.GenerateCollectors(t, lists)
+		coll.ASG.(*testdata.ASGClientMock).On("Delete", mock.Anything, mock.Anything).Return(nil)
+		coll.ASG.(*testdata.ASGClientMock).On("Complete", mock.Anything, mock.Anything).Return(nil)
 
-		ml := NewMainLoop(collectors)
+		ml := NewMainLoop(coll)
 
 		err := ml.runOnce(context.Background())
 		require.NoError(t, err)
 
-		collectors.ASG.(*testdata.ASGClientMock).AssertExpectations(t)
-		collectors.ASG.(*testdata.ASGClientMock).AssertNotCalled(t, "Complete", mock.Anything, mock.Anything)
+		if !coll.ASG.(*testdata.ASGClientMock).AssertExpectations(t) ||
+			!coll.ASG.(*testdata.ASGClientMock).AssertNotCalled(t, "Complete", mock.Anything, mock.Anything) {
+
+			instances, _ := collectors.Combine(lists)
+			for _, instance := range instances {
+				t.Logf("%#v", instance)
+			}
+		}
+
 	})
 }
